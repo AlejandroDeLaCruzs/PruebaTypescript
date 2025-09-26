8af51d8268b830f85fdff7d02e4c118226584556
AlejandroDeLaCruzs <alexcruzs1105@gmail.com>
2025-09-26 19:03:53 +0200

    express framework and movies api

--- index.ts
+++ index.ts
@@ -1,2 +1,22 @@
-const numeritos : number[] = [1,2,3,4, 5, 6]
-console.log(numeritos)
+import express, { type Request, type Response, type NextFunction } from "express";
+import fs from 'fs/promises'
+
+
+
+const app = express()
+
+app.get('/Movies', (req: Request,  res: Response) => {
+    fs.readFile("movies.json").then((data) => {
+        res.send(data)
+    })
+})
+
+//app.get('/Movies?id', (req))
+
+
+
+const port = process.env.PORT || 3000
+
+app.listen(port,  () => {
+    console.log(`http://localhost:${port}`)
+})
